aec5b2349ac533355544f6d607c617540b8e33f7
Rehan-priset2 <108051122+Rehan-priset2@users.noreply.github.com>
2022-06-28 06:24:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62838988839679'],
-  ['62838988839679'],
-  ['62838988839679', 'Owner', true]
+  ['6283898839679'],
+  ['6283898839679'],
+  ['6283898839679', 'OwnerHannOfc:v', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@hannn_bot'
+  var sticker_author = 'Rehan:v'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '© Rehan'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
